9721b17d171bcba4b99297ae14ca85ea8ce2c382
Jan Milosh <janmilosh@gmail.com>
2013-08-24 09:01:05 -0400

    Complete control logic

--- game.rb
+++ game.rb
@@ -21,7 +21,7 @@ end
 #        Method to prompt the player to roll or bank
 # ---------------------------------------------------------
 def prompt(player)
-	"#{player} rolled:  \bEnter b to end turn and bank score.\nEnter scoring dice numbers to keep rolling (ex: 1115)."
+	"#{player} rolling:  \bEnter b to end turn and bank score, or\nenter scoring dice numbers to keep rolling (ex: 1115)."
 end
 # ---------------------------------------------------------
 #      Method to roll dice and draw output in terminal
@@ -31,7 +31,6 @@ def roll_dice(n = 6)
 	@roll_total = 0
 	n.times do |i|
 		@die[i] = rand(6) + 1
-		@roll_total = @roll_total + @die[i]
 	end
 	puts
 	n.times do |i|
@@ -94,34 +93,43 @@ end
 #                Method to complete a round
 # ---------------------------------------------------------
 def round(player)
-	input = "playing"
+	input = ""
 	round_score = 0
 	number_of_dice = 6
-	begin																														# to bank your score
+	until input == "b"  #	you input b to bank your score and stop rolling																												
 		roll_dice(number_of_dice)
 		puts prompt(player)
-		input = gets.chomp.downcase																								# get the input ('b'ank or die numbers)																														# if you're not banking your score
-		score_array = input.split('').map { |x| x.to_i } 												# convert input to an array of integers
-		remaining_array = @die - score_array
-		score_count_array = score_array.group_by{|i| i}.map{|k,v| [k,v.count]}  # count the quantity of each number
-		score_hash = Hash[*score_count_array.flatten]														# convert to flattened hash
-		number_of_dice = @die.length - score_array.length
-		roll_score = get_score(score_hash)
+		input = gets.chomp  # get the input (b for bank or die numbers)
+		if input != "b"																																																					# if you're not banking your score
+			score_array = input.split('').map { |x| x.to_i } 	# convert input to an array of integers
+			remaining_array = @die - score_array
+			score_count_array = score_array.group_by{|i| i}.map{|k,v| [k,v.count]}  # count the quantity of each number
+			score_hash = Hash[*score_count_array.flatten]	 # convert to flattened hash
+			number_of_dice = @die.length - score_array.length
+			roll_score = get_score(score_hash)
+		else #calculate score when leaving this round
+			score_array = @die
+			score_count_array = score_array.group_by{|i| i}.map{|k,v| [k,v.count]}  # count the quantity of each number
+			score_hash = Hash[*score_count_array.flatten]	 # convert to flattened hash
+			roll_score = get_score(score_hash)
+		end
 		if roll_score == 0
 			round_score = 0
-			puts "You just Farkled and lost your points from this round."
+			puts "You just Farkled so you get zero points in this round."
+			puts "________________________________________________________"
+
 			break
 		else
 			round_score = round_score + roll_score
 		end
 		puts "Score for this roll: #{roll_score}"
 		puts "Score so far this round: #{round_score}"
-		puts "________________________________________________"
+		puts "________________________________________________________"
 		if number_of_dice == 0 && input != "b"
 			number_of_dice = 6
 			puts "You have hot dice!!!"
 		end
-	end while input != "b"
+	end
 	return round_score
 end
 # ---------------------------------------------------------
@@ -152,20 +160,19 @@ puts "Player one: What's your name?"
 player1 = Player.new(gets.chomp)
 puts "Player two: What's your name?"
 player2 = Player.new(gets.chomp)	
-puts "________________________________________________"
+puts "________________________________________________________"
 puts
 puts "Welcome to Farkle #{player1.name} and #{player2.name}!"
 puts "The game begins with #{player1.name} rolling first."
-puts "________________________________________________"
+puts "________________________________________________________"
 # ---------------------------------------------------------
 #          Play game method, start with player1
 # ---------------------------------------------------------
 player1.turn = true
 player = player1.name
 begin 
-	puts "#{player1.name}: #{player1.score} points   #{player2.name}: #{player2.score} points"
+	puts "#{player1.name} => #{player1.score} points   #{player2.name} => #{player2.score} points"
 	round_score = round(player)
-	puts "This is the round score #{round_score} for #{player}."
 	if player1.turn == true
 		player1.turn = false
 		player2.turn = true
@@ -182,7 +189,16 @@ begin
 	if quit == "q"
 		exit
 	end
-end while player1.score < 10000 && player2.score < 10000
+end while ((player1.score < 1000) and (player2.score < 1000))
+puts "#{player1.name} => #{player1.score} points   #{player2.name} => #{player2.score} points"
+puts "#{player} gets one last turn."
+round_score = round(player)
+if player1.turn == true
+	player1.score = player1.score + round_score
+else
+	player2.score = player2.score + round_score
+end
+puts "Final score: #{player1.name} => #{player1.score} points   #{player2.name} => #{player2.score} points"
 if player1.score > player2.score
 	puts "#{player1.name} wins!!!"
 elsif player2.score > player1.score
@@ -190,7 +206,7 @@ elsif player2.score > player1.score
 else
 	puts "It's a tie!!!"
 end
-
+puts "Thanks for playing Farkle, hope you come back soon!"
 
 
 
